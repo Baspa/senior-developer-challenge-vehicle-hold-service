f9d43ee4ff8a4671a00181d9efa84908ddcf3d09
Baspa <hello@baspa.dev>
2026-05-30 13:57:29 +0200

    feat: Vehicle model, factory, seeder

---
+++ database/factories/VehicleFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Vehicle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Vehicle>
+ */
+class VehicleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'vin' => Str::uuid()->toString(),
+            'name' => fake()->randomElement([
+                'DAF XF',
+                'DAF XG',
+                'DAF XG+',
+                'DAF CF',
+                'DAF LF',
+                'DAF 95',
+                'DAF 95XF',
+                'DAF 105',
+                'DAF 106',
+                'DAF FA'
+            ]),
+        ];
+    }
+}
